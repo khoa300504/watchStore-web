31c8da2c5f957d49cfad92d4a4890bd86355778e
khoa300504 <khoa300504@gmail.com>
2024-06-11 21:46:44 +0700

    Fix Absolute Path

--- src/components/CarouselSlide/index.jsx
+++ src/components/CarouselSlide/index.jsx
@@ -1,57 +1,66 @@
+import citizen from '~/assets/imgs/brands/citizen.jpg'
+import casio from '~/assets/imgs/brands/casio.jpg'
+import elio from '~/assets/imgs/brands/elio.jpg'
+import titan from '~/assets/imgs/brands/titan.jpg'
+import esprit from '~/assets/imgs/brands/esprit.jpg'
+import mvw from '~/assets/imgs/brands/mvw.jpg'
+import xiaomi from '~/assets/imgs/brands/xiaomi.jpg'
+import huwei from '~/assets/imgs/brands/huwei.jpg'
+
 function CarouselSlide() {
   return (
     <div className="overflow-hidden w-[85%] mb-16">
       <div className="flex animate-loopScroll">
         <ul className="flex items-center justify-center md:justify-start [&_img]:max-w-none">
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/citizen.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Citizen" />
+            <img src={citizen} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="citizen" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/casio.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="casio" />
+            <img src={casio} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="casio" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/elio.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Elio" />
+            <img src={elio} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Elio" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/titan.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Titan" />
+            <img src={titan} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Titan" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/esprit.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Esprit" />
+            <img src={esprit} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Esprit" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/mvw.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="mvw" />
+            <img src={mvw} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="mvw" />
           </li>
           <li className="brand-menu-item hidden x:block">
-            <img src="/src/assets/imgs/brands/xiaomi.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="xiaomi" />
+            <img src={xiaomi} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="xiaomi" />
           </li>
           <li className="brand-menu-item hidden x:block">
-            <img src="/src/assets/imgs/brands/huwei.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="huwei" />
+            <img src={huwei} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="huwei" />
           </li>
         </ul>
         <ul className="flex items-center justify-center md:justify-start [&_img]:max-w-none" aria-hidden="true">
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/citizen.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="citizen" />
+            <img src={citizen} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="citizen" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/casio.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="casio" />
+            <img src={casio} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="casio" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/elio.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Elio" />
+            <img src={elio} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Elio" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/titan.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Titan" />
+            <img src={titan} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Titan" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/esprit.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Esprit" />
+            <img src={esprit} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="Esprit" />
           </li>
           <li className="brand-menu-item">
-            <img src="/src/assets/imgs/brands/mvw.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="mvw" />
+            <img src={mvw} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="mvw" />
           </li>
           <li className="brand-menu-item hidden x:block">
-            <img src="/src/assets/imgs/brands/xiaomi.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="xiaomi" />
+            <img src={xiaomi} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="xiaomi" />
           </li>
           <li className="brand-menu-item hidden x:block">
-            <img src="/src/assets/imgs/brands/huwei.jpg" className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="huwei" />
+            <img src={huwei} className='w-[2.75rem] x:w-[4.68rem] lg:w-36' alt="huwei" />
           </li>
         </ul>
       </div>
